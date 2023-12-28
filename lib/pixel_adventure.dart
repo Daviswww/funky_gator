88837a7342fd438bf78f83208a0ebdb24238563a
davis_ho <davis_ho@perfectcorp.com>
2023-12-28 16:50:03 +0800

    Add world
    [Why] Sync with guideline
    [How] As titled.
    
    Auditors: wayne_yang

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,3 +1,30 @@
-import 'package:flame/game.dart';
+import 'dart:async';
 
-class PixelAdventure extends FlameGame {}
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:funky_gator/level.dart';
+
+class PixelAdventure extends FlameGame {
+  @override
+  Color backgroundColor() => Colors.amber;
+
+  @override
+  FutureOr<void> onLoad() {
+    world = Level();
+
+    camera = CameraComponent.withFixedResolution(
+      world: world,
+      width: 640,
+      height: 360,
+    );
+    camera.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([
+      camera,
+      world,
+    ]);
+
+    return super.onLoad();
+  }
+}
